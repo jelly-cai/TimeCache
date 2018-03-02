9f24f6599d1d2f61251df419656fc54a4b0d7969
sung <yanchao@xipintech.com>
2018-03-02 18:48:49 +0800

    增、删、查、改

--- timecache/src/main/java/com/jelly/db/TimeCacheDbHelper.java
+++ timecache/src/main/java/com/jelly/db/TimeCacheDbHelper.java
@@ -10,13 +10,30 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class TimeCacheDbHelper extends SQLiteOpenHelper{
 
-    public TimeCacheDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    private static  final String DATABASE_NAME ="timecache";
+
+    private static  final int VERSION = 1;
+
+    public static final String DATABASE_TABLE ="cache";
+
+    public TimeCacheDbHelper(Context context){
+        super(context, DATABASE_NAME, null, VERSION);
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        StringBuilder cacheBuffer = new StringBuilder();
+        cacheBuffer.append("create table if not exists" + " "
+                + DATABASE_TABLE); // 表名
+        cacheBuffer.append("(");
+        cacheBuffer.append("id INTEGER PRIMARY KEY AUTOINCREMENT,");
+        cacheBuffer.append("key varchar(60) unique,");
+        cacheBuffer.append("value text,");
+        cacheBuffer.append("savetime varchar(13),");
+        cacheBuffer.append("cachetime int(8)");
+        cacheBuffer.append(")");
+        db.execSQL(cacheBuffer.toString());
     }
 
     @Override
